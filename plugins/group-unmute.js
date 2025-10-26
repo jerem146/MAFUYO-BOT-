ebef551bd80b4edbfc1dfad1f6d5942574121b34
jerem146 <vasquezever146@gmail.com>
2025-10-26 18:11:37 -0500

    Crear group-unmute.js

---
+++ plugins/group-unmute.js
@@ -0,0 +1,23 @@
+import { isGroup } from "../lib/support.js" // Ajusta la ruta si es necesario o elimina si no usas
+
+let handler = async (m, { conn, isAdmin, isBotAdmin, args, usedPrefix, command, chat }) => {
+    if (!m.isGroup) return m.reply('『✦』Este comando solo puede ser usado en grupos.')
+    if (!isAdmin) return m.reply('『✦』Solo los administradores del grupo pueden usar este comando.')
+    if (!isBotAdmin) return m.reply('『✦』El bot debe ser administrador para ejecutar este comando.')
+
+    if (!chat.isMute) {
+        return m.reply(`『🔊』Este grupo no está silenciado.`)
+    }
+
+    chat.isMute = false
+    m.reply(`『🔊』Este grupo ha sido activado. Ahora los comandos están habilitados de nuevo para los miembros.`)
+}
+
+handler.help = ['unmute']
+handler.tags = ['group']
+handler.command = ['unmute']
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+
+export default handler
